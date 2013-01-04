f85bc7ce1ea7fd5d5307a8281aaf104afdaed632
Nathan Dunn <ndunn@me.com>
2013-01-04 15:14:08 -0800

    on select down go directly to link . . . .on return . . . should do loose search

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,20 @@
 import edu.uoregon.StubData
+import edu.uoregon.sticklebackdb.Stock
 
 class BootStrap {
 
     def init = { servletContext ->
 
-        new StubData().stubAll()
+        String bootstrapValue = System.properties["bootstrap"]
+        log.debug "bootstrapValue: " + bootstrapValue
+        if (bootstrapValue) {
+            new StubData().stubAll()
+        }
+        else
+        if(Stock.count()==0)
+        {
+            new StubData().stubAll()
+        }
 
 
     }
