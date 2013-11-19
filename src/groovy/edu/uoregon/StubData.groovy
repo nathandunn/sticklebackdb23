860fd3b29734648153de7cd29a205dda74bb63e2
Kristi Potter <kpotter@uoregon.edu>
2013-11-19 11:11:11 -0800

    more kp changes

--- src/groovy/edu/uoregon/StubData.groovy
+++ src/groovy/edu/uoregon/StubData.groovy
@@ -163,48 +163,40 @@ class StubData {
                 // Maternal stock & individual lineage (column W)
                 def maternalStockID =  tokens[22]?.size() > 0 ? tokens[22].toString() : null
                 if(maternalStockID != null){
+                                  
+                    // get the stock ID
+                    def sID = maternalStockID.split("\\.")[0] as Double
                     
-                    def splits = maternalStockID.split("\\.")   
-                   print "splits " + length(splits)
-                    
-                   // print "maternalStoclID " + maternalStockID
-                   // def sID = maternalStockID.split("\\.")[0]
-                   // println "sID " + sID
-                   // def sID = maternalStockID.toString()//.split("\\.")[0]
-                    //def iID = maternalStockID.split("\\.")[1] 
-                   // if(maternalStockID.split("\\.")[1] != null)
-                    //    iID = maternalStockID.split("\\.")[1]
-                    //else   
-                     //  iID = 0
-                    
-                   // print "iUD " +iID
-                   // individual.maternalStockID = String.format("%.4f", sID)
-                    //individual.maternalIndividualID = String.format("%.4f",  iID)
-                    //println "sID: " + individual.maternalStockID //+ " iID: " + individual.maternalIndividualID
-                    
+                    // get the individual ID
+                    def iID = maternalStockID as Double
+                                                        
+                   // set the values in the individual
+                   individual.maternalStockID = String.format("%.4f", sID)
+                   individual.maternalIndividualID = String.format("%.4f",  iID)                    
                 }
                 else{
                     individual.maternalStockID = null
                     individual.maternalIndividualID = null
                 }
-               
-                
+                           
                 // Paternal stock lineage (column X)
-            /*    def paternalStockID = tokens[23]?.size() > 0 ? tokens[23].toDouble() : null
+                def paternalStockID = tokens[23]?.size() > 0 ? tokens[23].toString() : null
                 if(paternalStockID != null){
-                    def sID = paternalStockID.toString().split("\\.")[0]
-                    def iID
-                    if(paternalStockID.toString().split("\\.")[1] != null)
-                        iId = paternalStockID.toString().split("\\.")[1]
-                    else
-                        iID = 0   
-                    individual.paternalStockID = String.format("%.4f", sID)
-                    individual.paternalIndividualID = String.format("%.4f", iID)
+                    
+                    // get the stock ID
+                    def sID = paternalStockID.split("\\.")[0] as Double
+                    
+                    // get the individual ID
+                    def iID = paternalStockID as Double
+                    
+                    // set the values in the individual
+                   individual.paternalStockID = String.format("%.4f", sID)
+                   individual.paternalIndividualID = String.format("%.4f",  iID)                   
                 }
                 else{
                     individual.paternalStockID = null
                     individual.paternalIndividualID = null
-                }*/
+                }
                 
                 // Fish location (column Q)
                 individual.fishLocation = tokens[16].size() > 0 ? tokens[16] : null
@@ -242,11 +234,8 @@ class StubData {
                     
                     // Get the maternal stock                  
                     if (individual.maternalStockID != null)  {  
-                        println "has stock ID " + individual.maternalStockID
-                        if(Stock.findByStockID(individual.maternalStockID) != null){
-                            println "found tsocl"
-                            individual.maternalStock = Stock.findByStockID(individual.maternalStockID)  
-                        }
+                        if(Stock.findByStockID(individual.maternalStockID) != null)
+                            individual.maternalStock = Stock.findByStockID(individual.maternalStockID)
                         else
                         individual.maternalStock = null
                     }
