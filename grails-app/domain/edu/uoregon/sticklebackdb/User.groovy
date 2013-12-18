03ce4ac8a8a0af04d09a40efd838d69105c18b38
Nathan Dunn <ndunn@me.com>
2013-12-18 15:30:16 -0800

    added new security piece

---
+++ grails-app/domain/edu/uoregon/sticklebackdb/User.groovy
@@ -0,0 +1,12 @@
+package edu.uoregon.sticklebackdb
+
+class User {
+    String username
+    String passwordHash
+    
+    static hasMany = [ roles: Role, permissions: String ]
+
+    static constraints = {
+        username(nullable: false, blank: false, unique: true)
+    }
+}
