ffc7fd4b8849a030010e0e4f4291c7f5471a73a2
Nathan Dunn <ndunn@uoregon.edu>
2014-03-21 12:49:40 -0700

    works, but smoething wrong with the backend

--- src/java/edu/uoregon/sticklebackdb/client/QuickEntryService.java
+++ src/java/edu/uoregon/sticklebackdb/client/QuickEntryService.java
@@ -5,13 +5,9 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("rpc")
 public interface QuickEntryService extends RemoteService {
-    String doit();
-
-    String getMeasuredValuesForExperiment(Integer experimentId);
-
-    String createMeasuredValue(Integer experimentId, String strain, String value, String category);
-
-    String removeMeasuredValue(Integer measuredValueId);
-
-    String saveMeasuredValue(Integer measuredValueId, String field, String oldValue, String newValue);
+    java.lang.String doit();
+    java.lang.String getMeasuredValuesForExperiment(java.lang.Integer arg0);
+    java.lang.String createMeasuredValue(java.lang.Integer arg0, java.lang.String arg1, java.lang.String arg2, java.lang.String arg3);
+    java.lang.String removeMeasuredValue(java.lang.Integer arg0);
+    java.lang.String saveMeasuredValue(java.lang.Integer arg0, java.lang.String arg1, java.lang.String arg2, java.lang.String arg3);
 }
--- src/java/edu/uoregon/sticklebackdb/client/QuickEntryServiceAsync.java
+++ src/java/edu/uoregon/sticklebackdb/client/QuickEntryServiceAsync.java
@@ -3,14 +3,9 @@ package edu.uoregon.sticklebackdb.client;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface QuickEntryServiceAsync {
-
     void doit(AsyncCallback callback);
-
-    void getMeasuredValuesForExperiment(Integer experimentId, AsyncCallback asyncCallback);
-
-    void createMeasuredValue(Integer experimentId, String strain, String value, String category, AsyncCallback asyncCallback);
-
-    void removeMeasuredValue(Integer measuredValueId, AsyncCallback<String> async);
-
-    void saveMeasuredValue(Integer measuredValueId, String field, String oldValue, String newValue, AsyncCallback<String> async);
+    void getMeasuredValuesForExperiment(java.lang.Integer arg0, AsyncCallback callback);
+    void createMeasuredValue(java.lang.Integer arg0, java.lang.String arg1, java.lang.String arg2, java.lang.String arg3, AsyncCallback callback);
+    void removeMeasuredValue(java.lang.Integer arg0, AsyncCallback callback);
+    void saveMeasuredValue(java.lang.Integer arg0, java.lang.String arg1, java.lang.String arg2, java.lang.String arg3, AsyncCallback callback);
 }
