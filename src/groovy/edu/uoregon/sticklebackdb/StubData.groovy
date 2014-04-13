9cbc8432eb1e9927de9ca02411be4bdfc72ff2bc
Nathan Dunn <ndunn@me.com>
2014-04-13 07:52:26 -0700

    fixed for materna and paternal

--- src/groovy/edu/uoregon/sticklebackdb/StubData.groovy
+++ src/groovy/edu/uoregon/sticklebackdb/StubData.groovy
@@ -231,30 +231,6 @@ class StubData {
                         } else {
                             stock.maternalIndividualID = null
                         }
-                        // Maternal stock ID (column R)
-//                        def maternalStockID = tokens[17].size() > 0 ? Math.round(tokens[117].toDouble()) : null
-//                        if (maternalStockID != null) {
-//                            stock.maternalStockID = maternalStockID
-//                        } else {
-//                            stock.maternalStockID = null
-//                        }
-                        // Paternal Individual ID (column T)
-                        /**
-                         * Should be of the form sbCrsXXX
-                         */
-//                        String paternalIndividualID = tokens[115].size() > 0 ? tokens[115] : null
-//                        if (paternalIndividualID != null) {
-//                            stock.paternalIndividualID = paternalIndividualID
-//                        } else {
-//                            stock.paternalIndividualID = null
-//                        }
-                        // Paternal stock ID (column U)
-//                        def paternalStockID = tokens[20].size() > 0 ? Math.round(tokens[20].toDouble()) : null
-//                        if (paternalStockID != null) {
-//                            stock.paternalStockID = paternalStockID
-//                        } else {
-//                            stock.paternalStockID = null
-//                        }
 
                         stock.save(flush: true, insert: true, failOnError: true)
                         if (stock.stockID == 108) {
@@ -300,45 +276,6 @@ class StubData {
                 // Stock (from column S)
                 individual.stock = Stock.findByStockID(individual.stockID)
 
-                // Fertlization date (column H)
-//                individual.fertilizationDate = tokens[7]?.size() > 0 ? Date.parse("mm/dd/yy", tokens[7]) : null
-
-                // Maternal stock & individual lineage (column W)
-//                def maternalStockID = tokens[22]?.size() > 0 ? tokens[22].toString() : null
-//                if (maternalStockID != null) {
-//
-//                    // get the stock ID
-//                    def sID = maternalStockID.split("\\.")[0] as Double
-//
-//                    // get the individual ID
-//                    def iID = maternalStockID as Double
-//
-//                    // set the values in the individual
-//                    individual.maternalStockID = sID
-//                    individual.maternalIndividualID = iID
-//                } else {
-//                    individual.maternalStockID = null
-//                    individual.maternalIndividualID = null
-//                }
-
-                // Paternal stock lineage (column X)
-//                def paternalStockID = tokens[23]?.size() > 0 ? tokens[23].toString() : null
-//                if (paternalStockID != null) {
-//
-//                    // get the stock ID
-//                    def sID = paternalStockID.split("\\.")[0] as Double
-//
-//                    // get the individual ID
-//                    def iID = paternalStockID as Double
-//
-//                    // set the values in the individual
-//                    individual.paternalStockID = sID
-//                    individual.paternalIndividualID = iID
-//                } else {
-//                    individual.paternalStockID = null
-//                    individual.paternalIndividualID = null
-//                }
-
                 // Fish location (column Q)
                 individual.somaLocation = tokens[16].size() > 0 ? tokens[16] : null
 
@@ -358,75 +295,11 @@ class StubData {
     }
 
     /*
-     * Process the lineage of individuals
-     */
-
-//    def processIndividualLineage() {
-//        CSVReader csvReader = getImportFile("individuals.csv").toCsvReader(skipLines: 1, 'charset': 'UTF-8')
-//
-//        println "start processing individuals"
-//        csvReader.eachLine { tokens ->
-////            if (tokens.size() > 15 && tokens[18]?.length() > 1 && tokens[19]?.length() > 1) {
-//            if (tokens.size() > 15 && tokens[18]?.length() > 0) {
-//
-//                // Get the individual object, if one exists
-//                Individual individual
-//
-//                // Get the individual
-//                if (tokens[20]?.length() > 1 && tokens[20].contains(".")) {
-//                    Integer individualID = tokens[20].split("\\.")[1] as Integer
-//                    individual = Individual.findByIndividualID(individualID)
-//                }
-//
-//                // If we have an individual
-//                if (individual) {
-//
-//                    // Get the maternal stock
-//                    if (individual.maternalStockID != null) {
-//                        if (Stock.findByStockID(individual.maternalStockID) != null)
-//                            individual.maternalStock = Stock.findByStockID(individual.maternalStockID)
-//                        else
-//                            individual.maternalStock = null
-//                    }
-//
-//                    // Get the paternal stock
-//                    if (individual.paternalStockID != null) {
-//                        if (Stock.findByStockID(individual.paternalStockID) != null)
-//                            individual.paternalStock = Stock.findByStockID(individual.paternalStockID)
-//                        else
-//                            individual.paternalStock = null
-//                    }
-//
-//                    // Get the maternal individual
-//                    if (individual.maternalIndividualID != null) {
-//                        if (Individual.findByIndividualID(individual.maternalIndividualID) != null)
-//                            individual.maternalIndividual = Individual.findByIndividualID(individual.maternalIndividualID)
-//                        else
-//                            individual.maternalIndividual = null
-//                    }
-//
-//                    // Get the paternal individual
-//                    if (individual.paternalIndividualID != null) {
-//                        if (Individual.findByIndividualID(individual.paternalIndividualID) != null)
-//                            individual.paternalIndividual = Individual.findByIndividualID(individual.paternalIndividualID)
-//                        else
-//                            individual.paternalIndividual = null
-//                    }
-//
-//                    individual.save(flush: true, insert: false)
-//                }
-//            }
-//        }
-//        println "finished processing Individuals ${Individual.count()} individuals added "
-//    }
-
-    /*
      * Process the lineage of stock
      */
-
     def processStockLineage() {
-        Map<String, String> crossMap = new HashMap<>()
-//        Map<String, String> maleMap = new HashMap<>()
+        Map<String, String> femaleMap = new HashMap<>()
+        Map<String, String> maleMap = new HashMap<>()
         CSVReader csvReader = getImportFile("crosses.csv").toCsvReader(skipLines: 1, 'charset': 'UTF-8')
         println "start processing stocks"
         csvReader.eachLine { tokens ->
@@ -435,11 +308,11 @@ class StubData {
                 if (tokens.size() > 5 && tokens[1]?.size() > 0) {
                     String key = tokens[17]
                     String individualID = tokens[1]
-//                    if (tokens[5] == "male") {
-//                        maleMap.put(key, individualID)
-//                    } else {
-                    crossMap.put(key, individualID)
-//                    }
+                    if (tokens[5] == "male") {
+                        maleMap.put(key, individualID)
+                    } else {
+                        femaleMap.put(key, individualID)
+                    }
                 }
 
 
@@ -451,123 +324,61 @@ class StubData {
 
         Stock.all.each { stock ->
 
-            if (stock.maternalIndividualID != null && crossMap.containsKey(stock.maternalIndividualID)) {
-                String individualIDValue = crossMap.get(stock.maternalIndividualID)
-                println "maternalID: ${individualIDValue}"
-                String[] ids = individualIDValue.split("\\.")
+            if (stock.maternalIndividualID != null) {
+                String individualIDValue
+                if (femaleMap.containsKey(stock.maternalIndividualID)) {
+                    individualIDValue = femaleMap.get(stock.maternalIndividualID)
+                    println "maternalID: ${individualIDValue}"
+                    String[] ids = individualIDValue.split("\\.")
 
-                Integer stockID = ids[0] as Integer
-                Stock maternalStock = Stock.findByStockID(stockID)
-                Integer individualID = ids[2] as Integer
+                    Integer stockID = ids[0] as Integer
+                    Stock maternalStock = Stock.findByStockID(stockID)
+                    Integer individualID = ids[2] as Integer
 
-                Individual individual = Individual.findByStockAndIndividualID(maternalStock, individualID)
-                if (individual) {
-                    println "individual found ${individual.individualIDLabel} mom of stock ${stock.stockIDLabel}"
-                    if (individual.fishSex == "female") {
-//                        println "fixing sex from ${individual.fishSex} to female"
-//                        individual.fishSex = "female"
+                    Individual individual = Individual.findByStockAndIndividualID(maternalStock, individualID)
+                    if (individual) {
+                        println "individual found ${individual.individualIDLabel} mom of stock ${stock.stockIDLabel}"
+                        if (individual.fishSex == "male") {
+                            println "fixing sex from ${individual.fishSex} to female"
+                            individual.fishSex = "female"
+                            individual.save()
+                        }
                         stock.maternalIndividual = individual
                         stock.maternalStock = individual.stock
+                        stock.save(flush: true)
                     } else {
-                        stock.paternalIndividual = individual
-                        stock.paternalStock = individual.stock
+                        println "no maternal individual found for ${individualIDValue}"
                     }
-                    stock.save(flush: true)
-                } else {
-                    println "no maternal individual found for ${individualIDValue}"
+                }
+                else
+                if (maleMap.containsKey(stock.maternalIndividualID)) {
+                    individualIDValue = maleMap.get(stock.maternalIndividualID)
+                    println "paternalID: ${individualIDValue}"
+                    String[] ids = individualIDValue.split("\\.")
+
+                    Integer stockID = ids[0] as Integer
+                    Stock paternalStock = Stock.findByStockID(stockID)
+                    Integer individualID = ids[2] as Integer
+
+                    Individual individual = Individual.findByStockAndIndividualID(paternalStock, individualID)
+                    if (individual) {
+                        println "individual found ${individual.individualIDLabel} mom of stock ${stock.stockIDLabel}"
+                        if (individual.fishSex == "female") {
+                            println "fixing sex from ${individual.fishSex} to female"
+                            individual.fishSex = "male"
+                            individual.save()
+                        }
+                        stock.maternalIndividual = individual
+                        stock.maternalStock = individual.stock
+                        stock.save(flush: true)
+                    } else {
+                        println "no paternal individual found for ${individualIDValue}"
+                    }
+                }
+                else {
+                    println "no cross ${stock.maternalIndividualID} found for stock ${stock.stockIDLabel}"
                 }
             }
-
-//            if (stock.paternalIndividualID != null && maleMap.containsKey(stock.paternalIndividualID)) {
-//                String individualIDValue = maleMap.get(stock.paternalIndividualID)
-//                println "paternalID: ${individualIDValue}"
-//                String[] ids = individualIDValue.split("\\.")
-//                Integer stockID = ids[0] as Integer
-//                Stock paternalStock = Stock.findByStockID(stockID)
-//                Integer individualID = ids[2] as Integer
-//                Individual individual = Individual.findByStockAndIndividualID(paternalStock, individualID)
-//
-//                if (individual) {
-//                    println "individual found ${individual.individualIDLabel} dad of stock ${stock.stockIDLabel}"
-//                    if (individual.fishSex != "male") {
-//                        println "fixing sex from ${individual.fishSex} to male"
-//                        individual.fishSex = "male"
-//                        individual.save()
-//                    }
-//                    if (individual) {
-//                        stock.paternalIndividual = individual
-//                        stock.paternalStock = individual.stock
-//                    }
-//                    stock.save(flush: true)
-//                } else {
-//                    println "no paternal individual found for ${individualIDValue}"
-//                }
-//            }
-
-//                    // Get the stock object
-//                    def stockID = tokens[14]?.size() > 0 ? tokens[14].toDouble() : null
-//                    Stock stock = Stock.findByStockID(stockID)
-//
-//                    // If we have a stock
-//                    if (stock) {
-//
-//                        // Get the maternal stock fron the saved ID
-//                        if (stock.maternalStockID != null) {
-//                            if (Stock.findByStockID(stock.maternalStockID) != null)
-//                                stock.maternalStock = Stock.findByStockID(stock.maternalStockID)
-//                        }
-//
-//                        // Get the paternal stock from the maternal stock ID
-//                        if (stock.paternalStockID != null) {
-//                            if (Stock.findByStockID(stock.paternalStockID) != null) {
-//                                stock.paternalStock = Stock.findByStockID(stock.paternalStockID)
-//                            }
-//                        }
-//
-//                        // Get the maternal individual
-//                        try {
-//                            if (stock.maternalIndividualID != null) {
-////                            Integer maternalStockID = stock.maternalIndividualID.split("\\.")[0] as Integer
-//
-//                                Integer maternalIndividualID
-//                                if (stock.maternalIndividualID.indexOf('.') > 0) {
-//                                    maternalIndividualID = stock.maternalIndividualID.split("\\.")[1] as Integer
-//                                } else {
-//                                    maternalIndividualID = stock.maternalIndividualID as Integer
-//                                }
-//
-////                                Integer maternalIndividualID = stock.maternalIndividualID.split("\\.")[1] as Integer
-////
-//                                if (Individual.findByIndividualIDAndStock(maternalIndividualID, stock.maternalStock) != null) {
-//                                    stock.maternalIndividual = Individual.findByIndividualIDAndStock(maternalIndividualID, stock.maternalStock)
-//                                }
-//                            }
-//                        }
-//                        catch (Exception me) {
-//                            println "failed to process maternal individual ${stock.maternalIndividualID} for stock ${stock.stockIDLabel} for: ${me.fillInStackTrace()}"
-//                        }
-//
-//                        // Get the paternal individual
-//                        try {
-//                            if (stock.paternalIndividualID != null) {
-//                                Integer paternalIndividualID
-//                                if (stock.paternalIndividualID.indexOf('.') > 0) {
-//                                    paternalIndividualID = stock.paternalIndividualID.split("\\.")[1] as Integer
-//                                } else {
-//                                    paternalIndividualID = stock.paternalIndividualID as Integer
-//                                }
-//
-//                                if (Individual.findByIndividualIDAndStock(paternalIndividualID, stock.paternalStock) != null) {
-//                                    stock.paternalIndividual = Individual.findByIndividualIDAndStock(paternalIndividualID, stock.paternalStock)
-//                                }
-//                            }
-//                        }
-//                        catch (Exception oe) {
-//                            println "failed to process paternal individual ${stock.paternalIndividualID} for stock ${stock.stockIDLabel} for ${oe.fillInStackTrace()}"
-//                        }
-//
-//                        stock.save(flush: true, insert: false)
-//                    }
         }
 
         println "finished processing stocks: " + Stock.count()
