83376446086b33f3b9dc822252c77a7d4150e064
Nathan Dunn <ndunn@cas.uoregon.edu>
2014-04-16 13:13:55 -0700

    updated produciton script

--- update_production.sh
+++ update_production.sh
@@ -2,14 +2,14 @@
 #hg pull -u 
 
 THIS_HOST=`hostname`
-#if [ "$THIS_HOST" = omero.uoregon.edu ]; then 
+if [ "$THIS_HOST" = omero.uoregon.edu ]; then 
 	grails clean
 	grails war 
-	sudo service tomcat7 stop
-	sudo rm -rf /var/lib/tomcat7/webapps/sticklebackdb*
-	sudo cp target/sticklebackdb-0.1.war /var/lib/tomcat7/webapps/sticklebackdb.war
+	sudo service tomcat6 stop
+	sudo rm -rf /var/lib/tomcat6/webapps/sticklebackdb*
+	sudo cp target/sticklebackdb-0.1.war /var/lib/tomcat6/webapps/sticklebackdb.war
 	sudo service tomcat7 start 
-#else
-#   echo "$THIS_HOST is not the production server" 
-#fi 
+else
+   echo "$THIS_HOST is not the production server" 
+fi 
 
